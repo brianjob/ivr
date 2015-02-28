42ed8d7ea9c62bbca0678a0140791382a2a775fc
Brian Barton <brianjob@umich.edu>
2015-02-27 23:30:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    session = require('express-session'),
+ bodyParser = require('body-parser'),
+        ivr = require('./ivr');
+
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(session({secret : process.env.SESSION_SECRET }));
+
+app.listen(port, function() {
+  console.log('listening on port ' + port);
+});
+
+app.get('/', function(req, res) {
+  var ivr_session = ivr.newSession(req);
+  res.send(ivr_session.response());
+});
+
+app.post('/', function(req, res) {
+  var ivr_session = ivr.resumeSession(req);
+  res.send(ivr_session.response());
+});
---
+++ ivr.js
@@ -0,0 +1,15 @@
+// EFFECTS:
+// 1. Looks up ivr settings based on number from request
+// 2. Creates a new ivr session
+// 3. Starts the session
+var newSession = function(req) {
+
+};
+
+// EFFECTS:
+// 1. Initializes an existing ivr session from a request object's session data
+// 2. Executes instructions for the current node
+// 3. Updates the current node
+var resumeSession = function(req) {
+
+};

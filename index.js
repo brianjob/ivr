62473e566be76b1a8838fdbf3101a74e68bb9776
Brian Barton <brianjob@umich.edu>
2015-03-05 16:26:53 -0500

    added procfile and I don't think we need the urlencoded body parser on second thought

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({secret : process.env.SESSION_SECRET }));
 
 app.use(function(err, req, res, next) {

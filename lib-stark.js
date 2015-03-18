09c26f330dacd5dc751112343538be4930681a37
Brian Barton <brianjob@umich.edu>
2015-03-18 13:50:10 -0400

    setStarkSourceKey()

--- lib-stark.js
+++ lib-stark.js
@@ -1,3 +1,16 @@
 module.exports.setStarkAccountID = function(model, input) {
   model.accountID = model.account_alpha + input;
 };
+
+// set the source key based on trust account
+module.exports.setStarkSourceKey = function(model) {
+  if (!model.usaepay_key_main) { throw new Error('main source key not set'); }
+  if (!model.usaepay_key_ford) { throw new Error('ford source key not set'); }
+  if (!model.auth.account) { throw new Error('account not yet authenticated'); }
+
+  if (model.auth.account.trust_account === 'FORD') {
+    model.usaepay_key = model.usaepay_key_ford;
+  } else {
+    model.usaepay_key = model.usaepay_key_main;
+  }
+};

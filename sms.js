a562e4f9b09acef8b2fca11a9e8e15c2d0ca28f8
Brian Barton <brianjob@umich.edu>
2015-03-19 17:56:30 -0400

    added sms.js

---
+++ sms.js
@@ -0,0 +1,19 @@
+var bars = require('handlebars');
+
+module.exports.run = function() {
+  if (!this.template) { throw new Error('sms node: ' + this.id + ' must have template'); }
+  if (!this.redirect) { throw new Error('sms node: ' + this.id + ' must have redirect'); }
+
+  var template = bars.compile(this.template);
+
+  this.ivr.twiml.sms({
+    to     : this.to,
+    from   : this.from,
+    action : '/sms',
+    method : 'POST'
+  }, template(this.ivr.model));
+
+  this.ivr.current_node = this.ivr.getNode(this.redirect);
+
+  return this.ivr.current_node.run();
+};

40d658cf6ea51d3977ba5d511a97746ed1054aeb
Brian Barton <brianjob@umich.edu>
2015-03-19 12:42:17 -0400

    updated rvo allow cc payment method

--- lib-rvo.js
+++ lib-rvo.js
@@ -4,7 +4,26 @@ var ach = function(model) {
 
 
 var cc = function(model) {
-  return model;
+  var allowed_creditor_ids = [
+    '328',
+    '454',
+    '2096',
+    '2116',
+    '2117',
+    '2226',
+    '2319',
+    '2320',
+    '2321',
+    '2322',
+    '2323',
+    '2492',
+    '2516',
+    '2522',
+    '2524',
+    '2526',
+    '2562'
+  ];
+    return allowed_creditor_ids.indexOf(model.auth.account.creditor_id) > -1;
 };
 
 module.exports.rvoAllowedACHandCC = function(model) {

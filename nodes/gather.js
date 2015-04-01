05978fdb65a5ce5ecc936250bb42a5d4f0b8e9e4
Brian Barton <brianjob@umich.edu>
2015-04-01 13:45:10 -0400

    PLAT-284 gather nodes

--- nodes/gather.js
+++ nodes/gather.js
@@ -7,18 +7,19 @@ module.exports.run = function() {
   if (!this.redirect) { throw new Error('gather node must have redirect'); }
   if (!this.numDigits && !this.finishOnKey) { throw new Error('gather node must have either numDigits or finishOnKey'); }
 
-  var self = this;
-  this.ivr.twiml.gather({
+  var gather_opts = { 
     action      : '/',
-    timeout     : this.timeout || this.ivr.default_timeout,
-    numDigits   : this.numDigits,
     finishOnKey : this.finishOnKey || ''
-  }, function() {
-    this.say({
-      voice    : self.voice    || self.ivr.default_voice,
-      language : self.language || self.ivr.default_language
-    }, self.prompt);
-  });
+  };
+
+  if (this.timeout || this.ivr.default_timeout) { gather_opts.timeout = this.timeout || this.ivr.default_timeout; }
+  if (this.numDigits) { gather_opts.numDigits = this.numDigits; }
+
+  var self = this;
+  var say_opts = {};
+  if (self.voice || self.ivr.default_voice) { say_opts.voice = self.voice || self.ivr.default_voice; }
+  if (self.language || self.ivr.default_language) { say_opts.language = self.language || self.ivr.default_language; }
+  this.ivr.twiml.gather(gather_opts, function() { this.say(say_opts, self.prompt); });
 
   this.ivr.twiml.redirect({
     method : 'POST'
